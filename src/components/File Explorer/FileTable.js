1e52f8ed0bc47fd4bc65ea4d7e97a05416d90521
teghmehta <t6ms17ov>
2019-03-05 15:09:01 -0500

    master

--- src/components/File Explorer/FileTable.js
+++ src/components/File Explorer/FileTable.js
@@ -5,12 +5,50 @@ import FileHeader from "./FileHeader";
 
 class FileTable extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state ={
+            fileData: this.props.fileData,
+        };
+    }
+
+    componentWillReceiveProps(nextProps, nextContext) {
+
+        this.setState({fileData: this.props.fileData})
+    }
+
+    onSort(key, type, direction) {
+        let data = this.props.fileData;
+        switch(type) {
+            case Date:
+                data.sort(function(a,b){
+                    return new Date(b.mod) - new Date(a.mod);
+                });
+                break;
+            case "":
+                data.sort((a,b) => a[key].localeCompare(b[key]));
+                break;
+            case 0:
+                data.sort(function(a,b){
+                    return b.size - a.size
+                });
+                break;
+            default:
+                data.sort((a,b) => a[key].localeCompare(b[key]));
+        }
+
+        if (direction === 'desc') {
+            data.reverse();
+        }
+        this.setState({fileData: data})
+    }
+
     render() {
         return (
             <div className="file-table-container">
                 <table className={"file-table"}>
                     <tbody>
-                    <FileHeader name={"Name"} mod={"Last Modified"} size={"Size"} desc={"Description"} />
+                    <FileHeader onSort={this.onSort.bind(this)} name={"Name"} mod={"Last Modified"} size={"Size"} desc={"Description"} />
                     <tr>
                         <th colSpan="5">
                             <hr/>
