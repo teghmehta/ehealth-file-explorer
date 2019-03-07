3ddeebb276456163f15661adeb11fb7158b66155
Tegh Mehta <tegh.mehta@gmail.com>
2019-03-07 15:05:30 -0500

    Fixed Bant Logo

--- src/components/Apps/AppsContainer.js
+++ src/components/Apps/AppsContainer.js
@@ -7,7 +7,7 @@ class AppsContainer extends Component {
         return (
             <div className="apps-container">
                 <AppDiv path="/medly" logoPath={require("../../res/MedlyS.png")} appName={"Medly"} isBantOrMedly={true}/>
-                <AppDiv path="/bant" logoPath={require("../../res/bant.png")} appName={"bant"} isBantOrMedly={true}/>
+                <AppDiv path="/bant" logoPath={require("../../res/logo-bant.svg")} appName={"bant"} isBantOrMedly={true}/>
                 <AppDiv path="/ned" logoPath={require("../../res/ned.JPG")} appName={"Ned"}/>
                 <AppDiv path="/breathe" logoPath={require("../../res/breathe.jpg")} appName={"breathe"}/>
                 <AppDiv path="/icc" logoPath={require("../../res/icc.jpg")} appName={"iCanCope"}/>
