e62d2ee0a2859845c5a6d166cab594ff7cd6c4b8
Tegh Mehta <tegh.mehta@gmail.com>
2019-03-08 15:14:08 -0500

    Added a basic functionality for quick links: Added the sortable table, the quick links title in the header and created new functions that give me dummy folders for Latest Release Folders

--- src/components/Apps/AppDiv.js
+++ src/components/Apps/AppDiv.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import './AppDiv.css';
 import {Link} from "react-router-dom";
+import QuickLinks from "../File Explorer/QuickLinks";
+import {getBuilds, getLatestReleaseFolders} from "../Dummy Data/DummyData";
+import FileTable from "../File Explorer/FileTable";
 
 class AppDiv extends Component {
     render() {
         return (
                 <div className="app-logo">
-                    <h2 className={"app-name"}>{this.props.appName}</h2>
-                    <Link to={this.props.path}>
-                        {this.props.isBantOrMedly ? (
-                            <img src={this.props.logoPath} alt={this.props.appName} className="app-logo-img bant-medly-sizing"/>
-                        ) : (
+                    <div className={"app-div-container"}>
+                        <Link to={this.props.path}>
+                            {/*<h2 className={"app-name"}>{this.props.appName}</h2>*/}
                             <img src={this.props.logoPath} alt={this.props.appName} className="app-logo-img"/>
-                        )}
-                    </Link>
+                        </Link>
+
+                    </div>
+                        <div className={"quick-links-container"}>
+                            <QuickLinks fileData={getLatestReleaseFolders("Latest Releases", "/", "/")}/>
+                        </div>
+
                 </div>
         );
     }
