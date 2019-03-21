bf7b6015e028c41c3d3e6025b9a9d782db0d665d
Tegh Mehta <tegh.mehta@gmail.com>
2019-03-21 15:43:06 -0400

    i just added some promt

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import AppsContainer from "./Apps/AppsContainer";
 import Header from "./Header/Header";
+import PopupBox from "../App";
 
 class Parent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state={show: true};
+    }
+
+    componentWillMount() {
+        let password;
+        do {
+
+            password = prompt("Enter your password: ");
+        } while(password !== "ehealth")
+    }
+
     render() {
         return (
-                <div className="App">
-                    <Header  title={"eHealth Innovations - App Download Links"} showTitle={true}/>
-                    <AppsContainer/>
-                </div>
+            <div className="App">
+                <Header  title={"eHealth Innovations - App Download Links"} showTitle={true}/>
+                <AppsContainer/>
+            </div>
         );
     }
 }
