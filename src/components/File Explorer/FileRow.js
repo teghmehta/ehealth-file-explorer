f28a82a28f18d02bf7b807286433089f42f936cd
Tegh Mehta <tegh.mehta@gmail.com>
2019-03-22 16:01:53 -0400

    Updated colors and added a highlight on hover

--- src/components/File Explorer/FileRow.js
+++ src/components/File Explorer/FileRow.js
@@ -8,9 +8,9 @@ class FileRow extends Component {
         return (
             <tr className={`file-row ${this.props.className}`}>
                 <td valign="top"><img src={this.props.icon} alt=""/></td>
-                <td><Link to={this.props.path}>{this.props.name}</Link></td>
-                <td>{this.props.mod}</td>
-                <td>{bytesToSize(this.props.size)}</td>
+                <td className={"td-name"}><Link to={this.props.path}>{this.props.name}</Link></td>
+                <td><Link to={this.props.path}>{this.props.mod}</Link></td>
+                <td><Link to={this.props.path}>{bytesToSize(this.props.size)}</Link></td>
                 {/*<td>{this.props.desc}</td>*/}
             </tr>
     );
